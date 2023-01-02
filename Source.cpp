33625dc353d0fc4e320035bdb02299ce9a4d44f4
Tyler Peaster <tpeaster@atu.edu>
2023-01-02 11:35:54 -0600

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+//Name: Tyler Peaster
+//Start Date: 1/2/2023
+
+/*
+C++ Console Blackjack Program
+Still early in dev
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello GitHub!\n";
+}
